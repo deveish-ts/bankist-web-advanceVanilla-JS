a7039195d9f04aafa757403000a37210c4c61a0c
ishraque-dev <ahamedesrak3@gmail.com>
2022-10-17 00:33:11 +0600

    computing usernames

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
+  sex: 'male',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -14,6 +15,7 @@ const account1 = {
 
 const account2 = {
   owner: 'Jessica Davis',
+  sex: 'female',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -21,6 +23,7 @@ const account2 = {
 
 const account3 = {
   owner: 'Steven Thomas Williams',
+  sex: 'male',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
@@ -28,6 +31,7 @@ const account3 = {
 
 const account4 = {
   owner: 'Sarah Smith',
+  sex: 'female',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -87,3 +91,20 @@ const displayMovements = (movements) => {
   });
 };
 displayMovements(account1.movements);
+// TODO: Computing user names.
+
+const createUserNames = (accounts) => {
+  accounts.forEach((account) => {
+    const gender = `${account.sex === 'male' ? 'Mr.' : 'Ms.'}`;
+
+    account.username = account.owner
+      .split(' ')
+      .map((item) => {
+        return item[0];
+      })
+      .join('');
+    account.username = gender + account.username;
+  });
+};
+createUserNames(accounts);
+console.log(accounts);
